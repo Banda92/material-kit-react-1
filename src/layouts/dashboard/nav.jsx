1adc213f8994ca8107a2f9c112861ded797bd8c0
Banda92 <qksdudgns92@gmail.com>
2024-04-08 11:07:52 +0900

    네비게이션, FAM수정
    1) 네비게이션 환자조회 입력요소 추가
    2) FloatingActionModal 사이즈 수정

--- src/layouts/dashboard/nav.jsx
+++ src/layouts/dashboard/nav.jsx
@@ -1,11 +1,12 @@
-import { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { useState, useEffect } from 'react';
 
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
 import Drawer from '@mui/material/Drawer';
 import Avatar from '@mui/material/Avatar';
 import { alpha } from '@mui/material/styles';
+import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 import ListItemButton from '@mui/material/ListItemButton';
 
@@ -23,12 +24,19 @@ import Scrollbar from 'src/components/scrollbar';
 import { NAV } from './config-layout';
 import navConfig from './config-navigation';
 
+
 // ----------------------------------------------------------------------
 
 export default function Nav({ openNav, onCloseNav }) {
   const pathname = usePathname();
 
   const upLg = useResponsive('up', 'lg');
+
+  const [patNo, setPatNo] = useState('')
+
+  const handlePatNo = (e) => { setPatNo(e.target.value) }
+
+  // useEffect(()=>{console.log(patNo)},[patNo])
 
   useEffect(() => {
     if (openNav) {
@@ -62,6 +70,31 @@ export default function Nav({ openNav, onCloseNav }) {
     </Box>
   );
 
+
+  const renderSearchPatientNumber = (
+    <Box
+      sx={{
+        // my: 3,
+        mx: 2.5,
+        // py: 2,
+        // px: 2.5,
+        display: 'flex',
+        borderRadius: 1.5,
+        alignItems: 'center',
+        // bgcolor: (theme) => alpha(theme.palette.grey[500], 0.12),
+      }}
+    >
+      <TextField
+        fullWidth // 필드가 그리드 셀의 전체 너비를 차지하도록 설정합니다.
+        label='환자번호 조회' // 레이블은 항목의 이름으로 설정됩니다.
+        value={patNo} // 값은 상태에서 관리되는 항목의 값입니다.
+        onChange={(e) => handlePatNo(e)} // 값이 변경될 때 handleChange를 호출합니다.
+      // name={item.name} // 각 필드를 식별하기 위한 이름으로 항목의 키를 사용합니다.
+      />
+    </Box>
+
+  )
+
   const renderSelectedPatientInfo = (
     <Box
       sx={{
@@ -75,12 +108,14 @@ export default function Nav({ openNav, onCloseNav }) {
         bgcolor: (theme) => alpha(theme.palette.grey[500], 0.12),
       }}
     >
-      
+
+
+
 
       <Box sx={{ ml: 2 }}>
         {/* <Typography variant="subtitle1">Selected Patient Info</Typography> */}
         <Typography variant="subtitle1" sx={{ color: 'text.secondary' }}>선택환자 정보</Typography>
-        <hr/>
+        <hr />
         <Typography variant="body2" sx={{ color: 'text.secondary' }}>환자번호: {patient.pid}</Typography>
         <Typography variant="body2" sx={{ color: 'text.secondary' }}>성별: {patient.sex}</Typography>
         <Typography variant="body2" sx={{ color: 'text.secondary' }}>나이: {patient.age}</Typography>
@@ -109,16 +144,16 @@ export default function Nav({ openNav, onCloseNav }) {
         <Box sx={{ textAlign: 'center' }}>
           <Typography variant="h6">Need Help?</Typography>
 
-          <Typography variant="h7" sx={{ mt: 1,}}>
-          Contact 
+          <Typography variant="h7" sx={{ mt: 1, }}>
+            Contact
           </Typography>
           <Typography variant="body2" sx={{ color: 'text.secondary', mt: 1 }}>
-          반영훈
+            반영훈
           </Typography>
           <Typography variant="body2" sx={{ color: 'text.secondary', mt: 1 }}>
-          010-7595-7666 qksdudgns92@gmail.com
+            010-7595-7666 qksdudgns92@gmail.com
           </Typography>
-          
+
         </Box>
 
         {/* <Button
@@ -147,6 +182,7 @@ export default function Nav({ openNav, onCloseNav }) {
       <Logo sx={{ mt: 3, ml: 4 }} />
 
       {renderAccount}
+      {renderSearchPatientNumber}
       {renderSelectedPatientInfo}
       {renderMenu}
 
