1ed892ef17ed48631d17526342dcca2d96a53710
Banda92 <qksdudgns92@gmail.com>
2024-04-09 16:14:07 +0900

    Update app-PatientInfo-Grid.jsx
    Make more flexible

--- src/sections/overview/app-PatientInfo-Grid.jsx
+++ src/sections/overview/app-PatientInfo-Grid.jsx
@@ -1,37 +1,37 @@
 import React from 'react';
 
-import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
+import { useTheme } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 function PatientInfoGrid() {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.up('md'));
+
   return (
     <Paper elevation={3} sx={{ p: 2, margin: 'auto', flexGrow: 1, display: 'flex', flexDirection: 'column' }}>
       <Typography variant="subtitle1" gutterBottom sx={{ mb: 2 }}>
         환자 정보
       </Typography>
-      <Grid container spacing={2} alignItems="center">
-        <Grid item xs={3}>
-          <Typography variant="body2" color="text.secondary">
-            환자번호: 0001
-          </Typography>
-        </Grid>
-        <Grid item xs={3}>
-          <Typography variant="body2" color="text.secondary">
-            나이: 35
-          </Typography>
-        </Grid>
-        <Grid item xs={3}>
-          <Typography variant="body2" color="text.secondary">
-            성별: Male
-          </Typography>
-        </Grid>
-        <Grid item xs={3}>
-          <Typography variant="body2" color="text.secondary">
-            몸무게: 75 kg
-          </Typography>
-        </Grid>
-      </Grid>
+      <div style={{
+        display: 'flex',
+        flexDirection: matches ? 'row' : 'column',
+        flexWrap: 'wrap'
+      }}>
+        <Typography variant="body2" color="text.secondary" sx={{ mb: 1, whiteSpace: 'normal', flex: matches ? '1 1 auto' : '1 0 100%' }}>
+          환자번호: 0001
+        </Typography>
+        <Typography variant="body2" color="text.secondary" sx={{ mb: 1, whiteSpace: 'normal', flex: matches ? '1 1 auto' : '1 0 100%' }}>
+          나이: 35
+        </Typography>
+        <Typography variant="body2" color="text.secondary" sx={{ mb: 1, whiteSpace: 'normal', flex: matches ? '1 1 auto' : '1 0 100%' }}>
+          성별: Male
+        </Typography>
+        <Typography variant="body2" color="text.secondary" sx={{ mb: 1, whiteSpace: 'normal', flex: matches ? '1 1 auto' : '1 0 100%' }}>
+          몸무게: 75 kg
+        </Typography>
+      </div>
     </Paper>
   );
 }
