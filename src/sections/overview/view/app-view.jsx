cd89570b19497a2dcdb75a1facda943872edb15b
Banda92 <qksdudgns92@gmail.com>
2024-06-20 13:21:18 +0900

    Update app-view.jsx
    레이아웃 정렬

--- src/sections/overview/view/app-view.jsx
+++ src/sections/overview/view/app-view.jsx
@@ -33,7 +33,7 @@ export default function AppView() {
   return (
     <Container maxWidth="xl" sx={{ height: `${innerHeight - 80 - innerHeight * 0.06}px` }}>
       <Grid container spacing={3} sx={{ height: '100%' }}>
-        <Grid item xs={12} sm={6} md={7} sx={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+        <Grid item xs={12} sm={6} md={7} sx={{ display: 'flex', flexDirection: 'column', height: '100%', gap:3}}>
           <Grid item sx={{ flexBasis: '70%', flexShrink: 0 }}>
             <AppCentralMonitor title="Central Monitor" />
           </Grid>
