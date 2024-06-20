3b363319931a0e414e173f305ae7d986f69b82f7
Banda92 <qksdudgns92@gmail.com>
2024-06-20 17:00:58 +0900

    Update app-central-monitor-data.jsx
    기초모델생성

--- src/sections/overview/app-central-monitor-data.jsx
+++ src/sections/overview/app-central-monitor-data.jsx
@@ -1,20 +1,96 @@
 import PropTypes from 'prop-types';
-
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
+import {
+  Box,
+  Card,
+  CardHeader,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper
+} from '@mui/material';
 
 // ----------------------------------------------------------------------
 
-export default function AppCentralMonitorData({ title, subheader,  ...other }) {
-  
+export default function AppCentralMonitorData({ title, subheader, ...other }) {
+  const labData = {
+    "LabData": [
+      {
+        "Index": 1,
+        "Date": "2024-06-20",
+        "Time": "10:00:00",
+        "Item": "HR",
+        "Code": "HR01",
+        "Value": 72
+      },
+      {
+        "Index": 2,
+        "Date": "2024-06-20",
+        "Time": "10:01:00",
+        "Item": "SPO2",
+        "Code": "SP01",
+        "Value": 98
+      },
+      {
+        "Index": 3,
+        "Date": "2024-06-20",
+        "Time": "10:02:00",
+        "Item": "HR",
+        "Code": "HR02",
+        "Value": 75
+      },
+      {
+        "Index": 4,
+        "Date": "2024-06-20",
+        "Time": "10:03:00",
+        "Item": "SPO2",
+        "Code": "SP02",
+        "Value": 97
+      },
+      {
+        "Index": 5,
+        "Date": "2024-06-20",
+        "Time": "10:04:00",
+        "Item": "HR",
+        "Code": "HR03",
+        "Value": 70
+      }
+    ]
+  };
 
   return (
-    <Card {...other} sx={{height:'100%'}}>
+    <Card {...other} sx={{ height: '100%' }}>
       <CardHeader title={title} subheader={subheader} />
 
       <Box sx={{ p: 3, pb: 1 }}>
-        1
+        <TableContainer component={Paper}>
+          <Table stickyHeader>
+            <TableHead>
+              <TableRow>
+                <TableCell>Index</TableCell>
+                <TableCell>Date</TableCell>
+                <TableCell>Time</TableCell>
+                <TableCell>Item</TableCell>
+                <TableCell>Code</TableCell>
+                <TableCell>Value</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {labData.LabData.map((row) => (
+                <TableRow key={row.Index}>
+                  <TableCell>{row.Index}</TableCell>
+                  <TableCell>{row.Date}</TableCell>
+                  <TableCell>{row.Time}</TableCell>
+                  <TableCell>{row.Item}</TableCell>
+                  <TableCell>{row.Code}</TableCell>
+                  <TableCell>{row.Value}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </Box>
     </Card>
   );
